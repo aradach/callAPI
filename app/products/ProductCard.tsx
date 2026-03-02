534e763f37aa77a4729a8ac77a7b0869795210f3
aradach <arada2005.ch@gmail.com>
2026-03-02 20:02:59 +0700

    add all

--- app/products/ProductCard.tsx
+++ app/products/ProductCard.tsx
@@ -13,14 +13,17 @@ const ProductCard = ({product} : ProductProps) => {
   return (
     
     <div className="flex flex-col items-center border-2 border-gray-400 rounded-md p-5 m-5">
-        <div className="flex justify-between pb-5">
+        <div className="flex flex-col items-center justify-center m-5">
             <Image src={product.image} alt="profilepic" width={200} height={300}></Image>
         </div>
-        <div className="flex flex-col items-start pb-5">
+        <div className="flex flex-col flex-1 items-center justify-center text-2xl">
             <h2 className="font-bold text-md ">{product.title}</h2>
         </div>
-        <div className="flex flex-col items-start">
-            <h2 className="font-bold text-md ">{product.price}</h2>
+        <div className="flex items-center justify-center pt-3 pb-5 text-3xl font-bold text-blue-600">
+            <h2 className="font-bold text-md ">{product.price} $</h2>
+        </div>
+         <div className="flex justify-center rounded-xl bg-green-400 w-50 h-10 text-xl cursor-pointer hover:opacity-80 transition-all">
+            <button className="font-bold text-md cursor-pointer">ADD TO CART</button>
         </div>
     </div>
   )
